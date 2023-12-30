0f21c452746164777ea091e209617d6d13b190b0
Oleksandr Yatsenko <iosiguides@gmail.com>
2023-12-30 23:13:25 +0200

    Added HW3 && Added template form && Added animation for star

---
+++ genius-homework-3/js/modal.js
@@ -0,0 +1,23 @@
+// const modal = document.querySelector(".backdrop");
+// const modalBtnOpen = document.querySelector(".modal-btn-open");
+// const modalBtnClose = document.querySelector(".modal-btn-close");
+//
+// const toggleModal = () => {
+//   modal.classList.toggle("is-hidden");
+// };
+//
+// modalBtnOpen.addEventListener("click", toggleModal);
+// modalBtnClose.addEventListener("click", toggleModal);
+
+  document.addEventListener("DOMContentLoaded", function() {
+  const modal = document.querySelector(".backdrop");
+  const modalBtnOpen = document.querySelector(".modal-btn-open");
+  const modalBtnClose = document.querySelector(".modal-btn-close");
+
+  const toggleModal = () => {
+    modal.classList.toggle("is-hidden");
+  };
+
+  modalBtnOpen.addEventListener("click", toggleModal);
+  modalBtnClose.addEventListener("click", toggleModal);
+});
